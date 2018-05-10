bb3d73c2d38189baece2cd2b5f86d56ae4b18680
Woobin Yoo <woobin@Woobinui-MacBook-Pro.local>
2018-05-10 17:54:59 +0900

    나라 클래스랑 퀴즈 클래스 만듬

---
+++ app/src/main/java/project/sk/robocode3/robot_project/Quiz.java
@@ -0,0 +1,32 @@
+package project.sk.robocode3.robot_project;
+
+public static class Quiz {
+    //public static  final int capitalNum=4;
+
+    //{"한국","일본","중국","미국"};
+
+    public static String[][] capital;
+    public static String[][] clothes;
+    public static String[][] money;
+    public static String[][] food;
+
+    public static void Quiz(){
+        capital[0] = new String[]{"서울","도쿄","베이징","워싱턴D.C."};
+        capital[1] = new String[]{R.drawable.seoul,R.drawable.tokyo,R.drawable.beijing,R.drawable.washington};;
+
+        clothes[0] = new String[]{};
+        clothes[1] = new String[]{};
+
+        money[0] = new String[]{};
+        money[1] = new String[]{};
+
+        food[0] = new String[]{};
+        food[1] = new String[]{};
+
+
+
+
+    }
+
+
+}
--- app/src/main/java/project/sk/robocode3/robot_project/SplashActivity.java
+++ app/src/main/java/project/sk/robocode3/robot_project/SplashActivity.java
@@ -6,6 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    /*
+    static String[] nation_name = new String[]{"한국","일본","중국","미국"};
+    static String[] nation_capital = new String[]{"서울","도쿄","베이징","워싱턴"};
+    static int[] nation_oid = new int[]{51814,51813,51811,51815};
+    static int[] nation_flag = new int[]{R.drawable.korea,R.drawable.japan,R.drawable.china,R.drawable.america};
+    static int[] capital_button = new int[]{R.drawable.seoul,R.drawable.tokyo,R.drawable.beijing,R.drawable.washington};
+     */
+
+    int[] index;
+    String[] name = new String[]{"한국","일본","중국","미국"};
+    String[] dae = new String[]{"아시아","아시아","아시아","북아메리카"};
+    String[] cho = new String[]{"ㅎㄱ","ㅇㅂ","ㅈㄱ","ㅁㄱ"};
+    int[] oid = new int[]{51814,51813,51811,51815};;
+    String[] color = new String[]{"아시아","아시아","아시아","북아메리카"};;
+    int[] flag = new int[]{R.drawable.korea,R.drawable.japan,R.drawable.china,R.drawable.america};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +31,10 @@ public class SplashActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
 
+        //나라객체초기화
+        for(int i = 0;i<name.length;i++){
+            CommonObject.nation[i] = new NationObject(i,this.name[i],this.dae[i],this.cho[i],this.oid[i],this.color[i],this.flag[i]);
+        }
     }
 
 }
